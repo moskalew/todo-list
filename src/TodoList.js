ff877396fc4c2c2a29fe7542b597e9a085067eeb
andrei moskalev <moskalev.web.dev@gmail.com>
2024-10-13 13:35:36 +0300

    TodoList

---
+++ src/TodoList.js
@@ -0,0 +1,51 @@
+// src/TodoList.js
+import React, { useEffect, useState } from 'react';
+import './TodoList.css';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Ошибка при загрузке данных');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Ошибка:', error);
+        setError('Не удалось загрузить данные');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Загрузка...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div className="todo-container">
+      <h1 className="todo-title">Список задач</h1>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id} className="todo-item">
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
